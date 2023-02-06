7263ced029b6e42d3e0c5523ce5dc38fb49bd775
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-02-06 11:30:48 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,66 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { filterReducer } from "./contacts/filterSlice";
-import { contactsReducer } from "./contacts/contactsSlice";
-import { authReducer } from "./auth/slice";
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {contactsReducer} from './contacts/contactsSlice'
+import { authReducer } from './auth/slice';
 
-import { combineReducers } from "@reduxjs/toolkit";
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-  auth: authReducer
-});
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
+
+
+// import { configureStore } from "@reduxjs/toolkit";
+
+// import storage from 'redux-persist/lib/storage';
+// import { filterReducer } from "./contacts/filterSlice";
+// import { contactsReducer } from "./contacts/contactsSlice";
+// import { authReducer } from "./auth/slice";
+
+// import { combineReducers } from "@reduxjs/toolkit";
+
+// const authPersistConfig = {
+//   key: 'auth',
+//   storage,
+//   whitelist: ['token'],
+// };
+
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filter: filterReducer,
+//   auth: persistReducer(authPersistConfig, authReducer),
+// });
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });
 
